ca66c48bcfe47841df77b0a7a18af016e0d4d7fa
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2017-04-11 16:12:31 -0700

    Add top n teams

--- make_charts.py
+++ make_charts.py
@@ -4,15 +4,12 @@ import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 import urllib2
 import time
+import numpy as np
 
-hou_urls = [
-  "https://my.firstinspires.org/myarea/index.lasso?page=teamlist&event_type=FRC&sort_teams=number&year=2017&event=cmptx&skip_teams=0",
-  "https://my.firstinspires.org/myarea/index.lasso?page=teamlist&event_type=FRC&sort_teams=number&year=2017&event=cmptx&skip_teams=250"
-]
-stl_urls = [
-  "https://my.firstinspires.org/myarea/index.lasso?page=teamlist&event_type=FRC&sort_teams=number&year=2017&event=cmpmo&skip_teams=0",
-  "https://my.firstinspires.org/myarea/index.lasso?page=teamlist&event_type=FRC&sort_teams=number&year=2017&event=cmpmo&skip_teams=250"
-]
+NUM_TOP_TEAMS = 40
+
+def make_url_for_event_and_skip(event, skip):
+  return "https://my.firstinspires.org/myarea/index.lasso?page=teamlist&event_type=FRC&sort_teams=number&year=2017&event=" + event + "&skip_teams=" + str(skip)
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 
@@ -28,14 +25,6 @@ def get_teams_from_urls(urls):
         teams.append(int(row.find("a").string))
   return teams
 
-# with open(fname) as f:
-#     content = f.readlines()
-
-conn = sqlite3.connect('oprs2017.db')
-
-# you may also want to remove whitespace characters like `\n` at the end of each line
-# teams = filter(None, [x.strip() for x in content])
-
 def getBest(team, stat):
   c = conn.execute('Select %s from oprs where team_number = ?;' % (stat), (team,))
   res = c.fetchall()
@@ -47,38 +36,111 @@ def getBest(team, stat):
     return 0
 
 def make_title(title, teams):
-  return title + " - " + time.strftime("%x") + " - " + str(len(teams)) + " teams"
-
-def make_plots(title, teams):
-  gears = {}
-  fuel = {}
-  auto_fuel = {}
-  takeoff = {}
-
-  for team in teams:
-    auto_fuel[team] = getBest(team, 'auto_fuel_high_count_opr')
-    fuel[team] = getBest(team, 'teleop_fuel_high_count_opr')
-    gears[team] = getBest(team, 'gear_count_opr')
-    takeoff[team] = getBest(team, 'teleop_takeoff_points_opr')
-
-  auto_fuel_vals = sorted(auto_fuel.values(), reverse=True)
-  fuel_vals = sorted(fuel.values(), reverse=True)
-  gears_val = sorted(gears.values(), reverse=True)
-  takeoff_val = sorted(takeoff.values(), reverse=True)
-
-  fig, axes = plt.subplots(2, 2)
-  axes[0, 0].plot(auto_fuel_vals)
-  axes[0, 0].set_title('Auto fuel OPR')
-  axes[0, 1].plot(fuel_vals)
-  axes[0, 1].set_title('Tele fuel OPR')
-  axes[1, 0].plot(gears_val)
-  axes[1, 0].set_title('Gear OPR')
-  axes[1, 1].plot(takeoff_val)
-  axes[1, 1].set_title('Climb OPR')
-  fig.suptitle(make_title(title, teams))
-  plt.show()
+  return title + " - " + time.strftime("%x")
 
 
+def make_plots(event_infos):
+  global_fig, g_axes = plt.subplots(2, 3)
+  g_axes[0, 0].set_title('Auto fuel OPR')
+  g_axes[0, 0].set_ylabel('# Balls')
+  g_axes[0, 0].set_xlabel('Percentile')
+  g_axes[0, 1].set_title('Tele fuel OPR')
+  g_axes[0, 1].set_ylabel('# Balls')
+  g_axes[0, 1].set_xlabel('Percentile')
+  g_axes[0, 2].set_title('Fuel OPR')
+  g_axes[0, 2].set_ylabel('# Points')
+  g_axes[0, 2].set_xlabel('Percentile')
+  g_axes[1, 0].set_title('Gear OPR')
+  g_axes[1, 0].set_ylabel('# Gears')
+  g_axes[1, 0].set_xlabel('Percentile')
+  g_axes[1, 1].set_title('Climb OPR')
+  g_axes[1, 1].set_ylabel('Climb points')
+  g_axes[1, 1].set_xlabel('Percentile')
 
-make_plots("HOU CMP", get_teams_from_urls(hou_urls))
-make_plots("STL CMP", get_teams_from_urls(stl_urls))
+  top_n_fig, top_n_axes = plt.subplots(2, 3)
+  top_n_axes[0, 0].set_title('Auto fuel OPR')
+  top_n_axes[0, 0].set_ylabel('# Balls')
+  top_n_axes[0, 0].set_xlabel('Team index')
+  top_n_axes[0, 1].set_title('Tele fuel OPR')
+  top_n_axes[0, 1].set_ylabel('# Balls')
+  top_n_axes[0, 1].set_xlabel('Team index')
+  top_n_axes[0, 2].set_title('Fuel OPR')
+  top_n_axes[0, 2].set_ylabel('# Points')
+  top_n_axes[0, 2].set_xlabel('Team index')
+  top_n_axes[1, 0].set_title('Gear OPR')
+  top_n_axes[1, 0].set_ylabel('# Gears')
+  top_n_axes[1, 0].set_xlabel('Team index')
+  top_n_axes[1, 1].set_title('Climb OPR')
+  top_n_axes[1, 1].set_ylabel('Climb points')
+  top_n_axes[1, 1].set_xlabel('Team index')
+
+  event_names = []
+  for event_info in event_infos:
+    gears = {}
+    fuel = {}
+    fuel_points = {}
+    auto_fuel = {}
+    takeoff = {}
+    event_name, teams = event_info
+    event_names.append(event_name)
+    for team in teams:
+      auto_fuel[team] = getBest(team, 'auto_fuel_high_count_opr')
+      fuel[team] = getBest(team, 'teleop_fuel_high_count_opr')
+      gears[team] = getBest(team, 'gear_count_opr')
+      takeoff[team] = getBest(team, 'teleop_takeoff_points_opr')
+      fuel_points[team] = (fuel[team] / 3.0) + auto_fuel[team]
+
+    auto_fuel_vals = sorted(auto_fuel.values(), reverse=True)
+    fuel_vals = sorted(fuel.values(), reverse=True)
+    gears_val = sorted(gears.values(), reverse=True)
+    takeoff_val = sorted(takeoff.values(), reverse=True)
+    fuel_points_val = sorted(fuel_points.values(), reverse=True)
+    percentiles = np.arange(0., 100.0, (100./float(len(teams))));
+
+    g_axes[0, 0].plot(percentiles, auto_fuel_vals)
+    g_axes[0, 1].plot(percentiles, fuel_vals)
+    g_axes[0, 2].plot(percentiles, fuel_points_val)
+    g_axes[1, 0].plot(percentiles, gears_val)
+    g_axes[1, 1].plot(percentiles, takeoff_val)
+
+
+    nums  = np.arange(0, NUM_TOP_TEAMS, 1);
+    top_n_axes[0, 0].plot(nums, auto_fuel_vals[0:NUM_TOP_TEAMS])
+    top_n_axes[0, 1].plot(nums, fuel_vals[0:NUM_TOP_TEAMS])
+    top_n_axes[0, 2].plot(nums, fuel_points_val[0:NUM_TOP_TEAMS])
+    top_n_axes[1, 0].plot(nums, gears_val[0:NUM_TOP_TEAMS])
+    top_n_axes[1, 1].plot(nums, takeoff_val[0:NUM_TOP_TEAMS])
+
+  g_axes[0, 0].legend(event_names, loc='upper right')
+  top_n_axes[0, 0].legend(event_names, loc='upper right')
+
+  global_fig.suptitle(make_title("Normalized OPR Comparison", teams))
+  top_n_fig.suptitle(make_title("Top " + str(NUM_TOP_TEAMS) + " OPR Comparison", teams))
+  plt.show(global_fig)
+  plt.show(top_n_fig)
+
+
+# main start here
+conn = sqlite3.connect('oprs2017.db')
+hou_urls = [
+  make_url_for_event_and_skip("cmptx", 0),
+  make_url_for_event_and_skip("cmptx", 250)
+]
+stl_urls = [
+  make_url_for_event_and_skip("cmpmo", 0),
+  make_url_for_event_and_skip("cmptx", 250)
+]
+other_events = [
+  ["SJ", "casj"],
+  ["LV", "nvlv"],
+  ["NECMP", "necmp"]
+]
+
+events = [
+  ["HOU CMP", get_teams_from_urls(hou_urls)],
+  ["STL CMP", get_teams_from_urls(stl_urls)],
+]
+for e in other_events:
+  events.append([e[0], get_teams_from_urls([make_url_for_event_and_skip(e[1], 0)])])
+
+make_plots(events)
